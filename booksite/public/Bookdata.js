9533c93939981e6b735519e0738465a59961a536
Colton Shepherd <cshep2012@gmail.com>
2023-10-05 13:16:01 -0600

    starting book website, making navbar

---
+++ booksite/public/Bookdata.js
@@ -0,0 +1,22 @@
+const Bookdata = [
+    {
+        name: "The Alchemyst",
+        author: "Michael Scott",
+        readstatus: "read"
+    },
+    {
+        name: "The Warlock",
+        author: "Michael Scott",
+        readstatus: "read"
+    },
+    {
+        name: "Foundation",
+        author: "Isaac Asimov",
+        readstatus: "reading"
+    },
+    {
+        name: "Real Analysis",
+        author: "Jay Cummings",
+        readstatus: "not read"
+    }
+]
